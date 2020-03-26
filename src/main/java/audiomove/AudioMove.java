2eb1aa144678359c73b813a54a5b0b5e6e69760b
Pedro teixeira <pedroteixeirabisognin@gmail.com>
2020-03-26 10:29:17 -0300

    Inserindo IBM Watson Speech to Text com erros

--- src/main/java/audiomove/AudioMove.java
+++ src/main/java/audiomove/AudioMove.java
@@ -1,7 +1,10 @@
 package audiomove;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
@@ -10,6 +13,12 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
+
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.watson.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.speech_to_text.v1.model.SpeechRecognitionResults;
+import com.ibm.watson.speech_to_text.v1.websocket.BaseRecognizeCallback;
 
 public class AudioMove {
 
@@ -56,7 +65,7 @@ public class AudioMove {
 
 		
 			try {
-				Thread.sleep(5000);
+				Thread.sleep(3000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -73,6 +82,42 @@ public class AudioMove {
 		}
 	
 	
+		IamAuthenticator authenticator = new IamAuthenticator("jQM01kY1cl-kdTZNdPY-5xxD1Oy2bLfP9TJ-1UY08-5s");
+		SpeechToText speechToText = new SpeechToText(authenticator);
+		speechToText.setServiceUrl("https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/88a2f24c-d86b-4d3e-8d64-4af1fd6165be");
+
+		try {
+		  RecognizeOptions recognizeOptions = new RecognizeOptions.Builder()
+		    .audio(new FileInputStream("C:\\Record\\Record2.wav"))
+		    .contentType("audio/wav")
+		    .model("pt-BR_BroadbandModel")
+		    .keywords(Arrays.asList("rato", "roeu", "roupa"))
+		    .keywordsThreshold((float) 0.5)
+		    .maxAlternatives(7)
+		    .build();
+
+		  BaseRecognizeCallback baseRecognizeCallback =
+		    new BaseRecognizeCallback() {
+
+		      @Override
+		      public void onTranscription
+		        (SpeechRecognitionResults speechRecognitionResults) {
+		          System.out.println(speechRecognitionResults);
+		      }
+
+		      @Override
+		      public void onDisconnected() {
+		        System.exit(0);
+		      }
+
+		    };
+
+		  speechToText.recognizeUsingWebSocket(recognizeOptions,
+		    baseRecognizeCallback);
+		} catch (FileNotFoundException e) {
+		  e.printStackTrace();
+		}
+
 		
 	
 	}
